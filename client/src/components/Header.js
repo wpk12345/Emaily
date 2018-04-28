004c33f639b67daaab15dc43a416cc8ce45dae4d
William King <wpk12345@gmail.com>
2018-04-27 23:04:07 -0400

    test test

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
       case false:
         return (
           <li>
-            <a href="/auth/google">Login With Google</a>
+            <a href="/auth/google">Login Witth Google</a>
           </li>
         );
       default:
